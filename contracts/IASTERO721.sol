c4bfe96a27b2add035f316d0ab464e072b8555f9
Tomoya Nagasawa <ocrybit@gmail.com>
2022-03-23 03:56:44 +0900

    add tokenURI to IASTERO721

--- contracts/IASTERO721.sol
+++ contracts/IASTERO721.sol
@@ -5,4 +5,5 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 interface IASTERO721 is IERC721 {
   function mint(address to, string memory arweave_tx) external returns (uint tokenId);
   
+  function tokenURI(uint256 tokenId) external view returns (string memory);
 }
